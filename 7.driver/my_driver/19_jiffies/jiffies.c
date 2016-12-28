e77c98a14d7707dee0709c0f0c3c64d1af30535d
nma1x <ningx.ma@intel.com>
2016-12-28 16:33:52 +0800

    19_jiffies

--- 7.driver/my_driver/19_jiffies/jiffies.c
+++ 7.driver/my_driver/19_jiffies/jiffies.c
@@ -3,71 +3,78 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
-#include <linux/time.h>
-#include <linux/rtc.h>
 #define N 128
 MODULE_LICENSE("GPL");
+
+#define JIFFIES_USE
+#ifdef JIFFIES_USE
+#include <linux/time.h>
+#include <linux/rtc.h>
+#endif
 
 char data[N];
 
 static int major = 250;
-static int minor = 0; 
+static int minor = 0;
 
+#ifdef JIFFIES_USE
 struct timeval tv;
 struct rtc_time tm;
+#endif
+
 static int hello_open(struct inode *inode,struct file *fl)
 {
 	printk("hello_open\n");
-	
+
+#ifdef JIFFIES_USE
 	do_gettimeofday(&tv);//获得内核时间的一个大整数
 	printk("tv_sec = %ld\n",tv.tv_sec);
 
 	rtc_time_to_tm(tv.tv_sec,&tm);
 	printk("%d-%d-%d %d:%d:%d\n",tm.tm_year + 1900,tm.tm_mon + 1,tm.tm_mday,\
 			tm.tm_hour + 8,tm.tm_min,tm.tm_sec);
+
+	printk("jiffies = %ld\n",jiffies);
+#endif
+
 	return 0;
 }
 
-static int hello_release (struct inode *inode, struct file *file)
+static int hello_release(struct inode *inode, struct file *file)
 {
 	printk("hello_release\n");
-
 	return 0;
 }
 
-static ssize_t hello_read (struct file *file, char __user *buf, size_t size, loff_t *loff)
+static ssize_t hello_read(struct file *file, char __user *buf, size_t size, loff_t *loff)
 {
-	if(size > N){
+	if (size > N)
 		size = N;
-	}
-	if(size < 0){
-		return EINVAL;  
-	}
+	if (size < 0)
+		return -EINVAL;
 
-	if(copy_to_user(buf,data,size)){
-		return ENOMEM;
-	}
+	if (copy_to_user(buf, data, size))
+		return -ENOMEM;
 
 	printk("hello_read\n");
 	return size;
-	
 }
 
-static ssize_t hello_write (struct file *file, const char __user *buff, size_t size, loff_t *loff)
+static ssize_t hello_write(struct file *file, const char __user *buff, size_t size, loff_t *loff)
 {
-	if(size > N)
+	if (size > N)
 		size = N;
-	if(size < 0)
-		return EINVAL;
+	if (size < 0)
+		return -EINVAL;
 
-	if(0 != copy_from_user(data,buff,20)){
-		return ENOMEM; 
-	}
+	if (0 != copy_from_user(data, buff, size))
+		return -ENOMEM;
 
 	printk("hello_write\n");
 	printk("hello_write : data = %s\n",data);
 	return size;
 }
+
 static struct cdev cdev;
 static struct file_operations hello_ops = {
 	.owner = THIS_MODULE,
@@ -77,23 +84,22 @@ static struct file_operations hello_ops = {
 	.release = hello_release
 };
 
-
 static int hello_init(void)
 {
 	int ret;
 
-	dev_t devno = MKDEV(major,minor);
-	ret = register_chrdev_region(devno,1,"hello");
-	if(0 != ret){
+	dev_t devno = MKDEV(major, minor);
+	ret = register_chrdev_region(devno, 1, "hello");
+	if (0 != ret) {
 		printk("register_chrdev_region : error\n");
 		return -1;
 	}
 
-	cdev_init(&cdev,&hello_ops);
-	ret = cdev_add(&cdev,devno,1);
-	if(0 != ret){
+	cdev_init(&cdev, &hello_ops);
+	ret = cdev_add(&cdev, devno, 1);
+	if (0 != ret) {
 		printk("cdev_add\n");
-		unregister_chrdev_region(devno,1);
+		unregister_chrdev_region(devno, 1);
 		return -1;
 	}
 
@@ -103,17 +109,12 @@ static int hello_init(void)
 
 static void hello_exit(void)
 {
-
-	dev_t devno = MKDEV(major,minor);
+	dev_t devno = MKDEV(major, minor);
 	cdev_del(&cdev);
-	unregister_chrdev_region(devno,1);
+	unregister_chrdev_region(devno, 1);
 
-	printk("hello_exit\n");	
-
+	printk("hello_exit\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-
-
--- 7.driver/my_driver/19_jiffies/test.c
+++ 7.driver/my_driver/19_jiffies/test.c
@@ -16,23 +16,19 @@ int main(int argc, const char *argv[])
 	int nbyte;
 	char buff[20] = "hello world";
 
-	fd = open("/home/linux/my_driver/my_node/mod5_hello_write",O_RDWR,0664);	
-	if(0 > fd){
+	fd = open("/home/linux/my_driver/my_node/mod5_hello_write", O_RDWR, 0664);
+	if (0 > fd) {
 		printf("test : open : error\n");
 		return -1;
 	}
-	
-	write(fd,buff,20);
-    nbyte = read(fd,buff, sizeof(buff));
 
-	printf("test : buff[] = %s\n",buff);
-	printf("test : nbyte = %d\n",nbyte);
+	write(fd, buff, 20);
+	nbyte = read(fd,buff, sizeof(buff));
+
+	printf("test : buff[] = %s\n", buff);
+	printf("test : nbyte = %d\n", nbyte);
 
 	close(fd);
 
 	return 0;
-
 }
-
-
-
