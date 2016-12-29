6d5db645685525597809be98e8b914ec4e492c6d
nma1x <ningx.ma@intel.com>
2016-12-29 09:58:46 +0800

    22

--- 7.driver/my_driver/22_pwm_music/beep.c
+++ 7.driver/my_driver/22_pwm_music/beep.c
@@ -9,11 +9,12 @@
 #include <linux/sched.h>
 #include <linux/poll.h>
 #include <asm/io.h>
+MODULE_LICENSE("GPL");
 
 #define N 128
 #define num_of_device 4
-#define GPG3CON  0xE03001C0 //LED：gpg3con寄存器物理地址的宏定义
-#define GPG3DAT  0xE03001C4
+#define GPG3CON 0xE03001C0 //LED：gpg3con寄存器物理地址的宏定义
+#define GPG3DAT 0xE03001C4
 
 /*pwm和beep相关寄存器*/
 #define GPDCON 0xE0300080 /*beep相连引脚*/
@@ -34,20 +35,13 @@ unsigned int *tcon;
 unsigned int *tcntb1;
 unsigned int *tcmpb1;
 
-
-
-
-MODULE_LICENSE("GPL");
-
 char data[N] = "\0";
 
 static int major = 250;
 static int minor = 0;
 
-
 static struct class *cls;
 static struct device *device;
-
 
 int flag_rw = 1; //写
 static wait_queue_head_t hello_readq; //定义“等待队列头”
@@ -61,38 +55,32 @@ static int hello_open(struct inode *inode,struct file *file)
 	return 0;
 }
 
-static int hello_release (struct inode *inode, struct file *file)
+static int hello_release(struct inode *inode, struct file *file)
 {
-	//	flag ++;
-
 	printk("hello_release\n");
-
 	return 0;
 }
 
-static ssize_t hello_read (struct file *file, char __user *buf, size_t size, loff_t *loff)
+static ssize_t hello_read(struct file *file, char __user *buf, size_t size, loff_t *loff)
 {
-	if(size > N){
+	if(size > N)
 		size = N;
-	}
-	if(size < 0){
-		return -EINVAL;  
-	}
+	if(size < 0)
+		return -EINVAL;
 
 	wait_event_interruptible(hello_readq,flag_rw != 1); //等待事件，flag_rw != 1 唤醒条件
 
-	if(copy_to_user(buf,data,size)){
+	if(copy_to_user(buf,data,size))
 		return -ENOMEM;
-	}
 
 	flag_rw = 1;
 	wake_up_interruptible(&hello_writeq);
+
 	printk("hello_read\n");
 	return size;
-
 }
 
-static ssize_t hello_write (struct file *file, const char __user *buff, size_t size, loff_t *loff)
+static ssize_t hello_write(struct file *file, const char __user *buff, size_t size, loff_t *loff)
 {
 	wait_event_interruptible(hello_readq,flag_rw != 0);
 
@@ -101,28 +89,27 @@ static ssize_t hello_write (struct file *file, const char __user *buff, size_t s
 	if(size < 0)
 		return -EINVAL;
 
-	if(0 != copy_from_user(data,buff,20)){
-		return -ENOMEM; 
-	}
+	if(copy_from_user(data,buff, size))
+		return -ENOMEM;
 
 	flag_rw = 0;
 	wake_up_interruptible(&hello_readq); //唤醒队列
 
 	kill_fasync(&fap,SIGIO,POLLIN);
+
 	printk("hello_write\n");
 	printk("data = %s\n",data);
 	return size;
 }
-static long hello_unlocked_ioctl (struct file *file, unsigned int cmd, unsigned long arg)
+
+static long hello_unlocked_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int var = 0; //定义接受传参的变量，表示对第var个led灯操作
 	int ret;
 	ret = copy_from_user(&var,(void *)arg,sizeof(arg)); //接受应用层iocntl()传来的参数
 
-	switch(cmd){
-
+	switch(cmd) {
 	case PWM_ON:
-
 		writel((readl(tcon) & (~0xf << 8)) | (0x9 << 8),tcon);
 		break;
 	case SET_PRE:
@@ -132,32 +119,27 @@ static long hello_unlocked_ioctl (struct file *file, unsigned int cmd, unsigned 
 		writel(var,tcntb1);
 		writel(var >> 1,tcmpb1);
 		break;
-
-
-
 	}
 	printk("hello_unlocked_ioctl\n");
-
 	return 0;
 }
 
-static unsigned int hello_poll (struct file *file, struct poll_table_struct *table)
+static unsigned int hello_poll(struct file *file, struct poll_table_struct *table)
 {
 	int mask = 0;
+
 	poll_wait(file,&hello_readq,table);
 	poll_wait(file,&hello_writeq,table);
 
-	if(flag_rw != 0){
+	if(flag_rw != 0)
 		mask |= POLLOUT;
-	}
 	if(flag_rw != 1)
-	{
 		mask |= POLLIN;
-	}
+
 	return mask;
 }
 
-static int hello_fasync (int fd, struct file *file, int on)
+static int hello_fasync(int fd, struct file *file, int on)
 {
 	return (fasync_helper(fd,file,on,&fap));
 }
@@ -181,57 +163,57 @@ static int hello_init(void) //自定义加载函数
 
 	ret = register_chrdev_region(devno,num_of_device,"xhello"); //注册设备号
 	if(0 != ret){
-		//		alloc_chrdev_region(&devno,0,1,DEV_NAME); //自动分配设备号
+		//alloc_chrdev_region(&devno,0,1,DEV_NAME); //自动分配设备号
 		printk("register_chrdev_region : error\n");
 		return -1;
 	}
 
 	cdev_init(&cdev,&hello_ops); //初始化cdev结构体
 	ret = cdev_add(&cdev,devno,num_of_device); //注册cdev结构体
-	if(0 != ret){
+	if(0 != ret) {
 		printk("cdev_add fail\n");
-
 		goto err1;
 	}
 
 	cls = class_create(THIS_MODULE,"xhello"); //创建一个类
-	device_create(cls, device,devno + 0,NULL,"xhello-0"); //创建设备结点，次设备号加1
-	device_create(cls, device,devno + 1,NULL,"xhello-1");
-	device_create(cls, device,devno + 2,NULL,"xhello-2");
-	device_create(cls, device,devno + 3,NULL,"xhello-3");
+	device_create(cls, device, devno + 0, NULL, "xhello-0"); //创建设备结点，次设备号加1
+	device_create(cls, device, devno + 1, NULL, "xhello-1");
+	device_create(cls, device, devno + 2, NULL, "xhello-2");
+	device_create(cls, device, devno + 3, NULL, "xhello-3");
 
 	//	spin_lock_init(&lock); //初始化自旋锁
 	init_waitqueue_head(&hello_readq); //初始化“等待队列头”
 	init_waitqueue_head(&hello_writeq); //初始化“等待队列头”
 
 	gpg3con = ioremap(GPG3CON,4); //LED
-	if(NULL == gpg3con){
+	if(NULL == gpg3con) {
 		printk("pgp3con ioremap fail\n");
 		goto err2;
 	}
+
 	gpg3dat = ioremap(GPG3DAT,4);
-	if(NULL == gpg3dat){
+	if(NULL == gpg3dat) {
 		printk("gpg3dat ioremap fail\n");
 		goto err3;
 	}
 
 	//	*gpg3con = ((*gpg3con) & (~ 0xffff)) | 0x1111;
-	//	*gpg3dat = ((*gpg3dat) & (~0xf)) | 0xf;	
+	//	*gpg3dat = ((*gpg3dat) & (~0xf)) | 0xf;
 	writel((readl(gpg3con) & (~ 0xffff)) | 0x1111,gpg3con);
 	writel(readl(gpg3dat) | 0xf,gpg3dat);
 
 	/*pwm 和beep相关寄存器虚拟地址映射*/
-	gpdcon = ioremap(GPDCON,4);
-	tcfg0= ioremap(TCFG0,4);
-	tcfg1= ioremap(TCFG1,4);
-	tcon= ioremap(TCON,4);
-	tcntb1= ioremap(TCNTB1,4);
-	tcmpb1= ioremap(TCMPB1,4);
+	gpdcon = ioremap(GPDCON, 4);
+	tcfg0= ioremap(TCFG0, 4);
+	tcfg1= ioremap(TCFG1, 4);
+	tcon= ioremap(TCON, 4);
+	tcntb1= ioremap(TCNTB1, 4);
+	tcmpb1= ioremap(TCMPB1, 4);
 
 #if 0
 	/*pwm和beep相关寄存器初始化*/
 	writel((readl(gpdcon) & (~0xf << 4)) | (0x2 << 4),gpdcon); /*beep相连引脚为GDP1，gdpcon的[7:4]
-																设置为TOUT_1*/
+																 设置为TOUT_1*/
 	writel((readl(tcfg0) & (~0xff)) | 0xff,tcfg0); /*tcfg0*/
 	writel((readl(tcfg1) & (~0xf << 4)) | (0x2 << 4),tcfg1);
 	writel((readl(tcntb1) & (~0xffffffff)) | 0x200,tcntb1);
@@ -273,12 +255,8 @@ static void hello_exit(void) //自定义卸载函数
 	cdev_del(&cdev); //卸载cdev结构体
 	unregister_chrdev_region(devno,num_of_device); //卸载设备号
 
-	printk("hello_exit\n");	
-
+	printk("hello_exit\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-
-
--- 7.driver/my_driver/22_pwm_music/pwm_music.c
+++ 7.driver/my_driver/22_pwm_music/pwm_music.c
@@ -20,37 +20,37 @@ int main()
 	int dev_fd;
 	int div;
 	int pre = 255;
+
 	dev_fd = open("/dev/pwm",O_RDWR | O_NONBLOCK);
-	if ( dev_fd == -1 ) {
+	if (dev_fd == -1) {
 		perror("open");
 		exit(1);
 	}
 
-	ioctl(dev_fd,PWM_ON);
-	ioctl(dev_fd,SET_PRE,&pre);
+	ioctl(dev_fd, PWM_ON);
+	ioctl(dev_fd, SET_PRE, &pre);
+
 #if 0
-	for(i = 0;i<sizeof(MumIsTheBestInTheWorld)/sizeof(Note);i++ )
-	{
+	for (i = 0; i < sizeof(MumIsTheBestInTheWorld)/sizeof(Note); i++) {
 		div = (PCLK/256/4)/(MumIsTheBestInTheWorld[i].pitch);
 		ioctl(dev_fd, SET_CNT, &div);
-		usleep(MumIsTheBestInTheWorld[i].dimation * 50); 
+		usleep(MumIsTheBestInTheWorld[i]. dimation * 50);
 	}
 #endif
+
 #if 0
-	for(i = 0;i<sizeof(GreatlyLongNow)/sizeof(Note);i++ )
-	{
+	for (i = 0; i<sizeof(GreatlyLongNow)/sizeof(Note); i++) {
 		div = (PCLK/256/4)/(GreatlyLongNow[i].pitch);
 		ioctl(dev_fd, SET_CNT, &div);
-		usleep(GreatlyLongNow[i].dimation * 50); 
+		usleep(GreatlyLongNow[i]. dimation * 50);
 	}
 #endif
 
 #if 1
-	for(i = 0;i<sizeof(FishBoat)/sizeof(Note);i++ )
-	{
+	for (i = 0; i<sizeof(FishBoat)/sizeof(Note); i++) {
 		div = (PCLK/256/4)/(FishBoat[i].pitch);
 		ioctl(dev_fd, SET_CNT, &div);
-		usleep(FishBoat[i].dimation * 50); 
+		usleep(FishBoat[i]. dimation * 50);
 	}
 #endif
 	return 0;
